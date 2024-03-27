e9a5f1f47d831b9a52d187cc842e9f6348927899
ilyassebou <bouaarrouda.ilyasse@gmail.com>
2024-03-27 15:25:58 -0700

    Is perfect

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,23 +1,29 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_is_perfect - Checks if a binary tree is perfect.
- * @tree: Pointer to the root node of the tree to check.
- * Return: 1 if the tree and all nodes below it have 2 children each,
- * 0 otherwise.
+ * binary_tree_is_perfect - Determines whether a binary tree is perfect.
+ * A binary tree is perfect if all non-leaf nodes have exactly two children.
+ * @tree: The root node of the tree to check.
+ * Return: Returns 1 if tree and all nodes below it each have 2 children,
+ * otherwise returns 0. Returns 0 if tree is NULL.
  */
 
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	if (tree == NULL)
+	if (!tree)
 		return (0);
 
-	if ((tree->left == NULL && tree->right == NULL) ||
-			(tree->left && tree->right))
+	if (tree->right && tree->left)
 	{
+		if ((!tree->left->left && !tree->left->right) &&
+		    (!tree->right->left && !tree->right->right))
+			return (1);
+
 		return (binary_tree_is_perfect(tree->left) &&
-				binary_tree_is_perfect(tree->right));
+			binary_tree_is_perfect(tree->right));
 	}
+	else if (!(tree->parent))
+		return (1);
 
 	return (0);
 }
